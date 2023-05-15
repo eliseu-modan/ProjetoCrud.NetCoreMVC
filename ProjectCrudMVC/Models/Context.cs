e8e3be24d39de9d4c3ac5b1e628f3bb37fe2c031
Eliseu Junior <eliseuservice14@gmail.com>
2023-05-15 12:12:18 -0300

    SCharp .NetCoreMVC

---
+++ ProjectCrudMVC/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectCrudMVC.Models
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options) 
+        {
+        
+        
+        }
+
+        public DbSet<Product> Products { get; set; }    
+    }
+}
---
+++ ProjectCrudMVC/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectCrudMVC.Models
+{
+
+    [Table("Product")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+
+        [Column("Name")]
+       [Display(Name= "Nome")]
+        public string Name { get; set; }
+    }
+}
